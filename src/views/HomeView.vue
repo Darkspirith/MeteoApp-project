0ba09f9e401fb943a9add08c025fb64571a10e70
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-21 23:23:10 +0200

    HomeView update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
- 
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input />
+    </div>
   </main>
 </template>
+
+<script setup></script>
