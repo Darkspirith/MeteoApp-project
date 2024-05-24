229740a898ea273671db8a70265198b86adbd270
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-24 23:04:28 +0200

    tailwind.config.js update

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,7 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
-      // lg: "1024px",
-      // xl: "1280px",
-      // "2xl": "1536px",
+      lg: "1024px",
     },
   },
   plugins: [],
