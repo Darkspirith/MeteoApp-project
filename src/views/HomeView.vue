3e5a884d767c2343cf3248dea6396b4b19540ba3
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-26 00:14:06 +0200

    Add location search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,22 @@
       <input
         type="text"
         v-model="searchQuery"
-        @input="getSearchResults.value"
+        @input="getSearchResults"
         placeholder="Wpisz nazwę miejscowości..."
-        class="py-2 px-5 w-full rounded-lg bg-transparent border focus:border-meteo-secondary focus:outline "
+        class="py-2 px-5 w-full rounded-lg bg-transparent border focus:border-meteo-secondary focus:outline"
       />
       <ul
-        class="absolute bg-emerald-900 text-white w-full shadow-md py-2 px-1 top-[66px]"
+        class="absolute rounded-md bg-emerald-900 text-white w-full shadow-md py-2 px-1 top-[72px]"
+        v-if="geocodingResults"
       >
         <li
           v-for="searchResult in geocodingResults"
           :key="searchResult.id"
           class="py-2 cursor-pointer"
-        ></li>
+          @click="previewLocation(searchResult)"
+        >
+          {{ searchResult.local_name }}
+        </li>
       </ul>
     </div>
   </main>
@@ -25,21 +29,29 @@
 import axios from "axios";
 import { ref } from "vue";
 
+const openWeatherAPIKey = "2c668029f81081120d7a7b0a60dff86a";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const geocodingResults = ref(null);
+const searchError = ref(false);
 
 const getSearchResults = () => {
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${searchQuery.value}&count=20&language=en&format=json`
-      );
-      geocodingResults.value = result.data.features;
-      console.log(geocodingResults.value);
+      try {
+        const result = await axios.get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${searchQuery.value}&appid=${openWeatherAPIKey}`
+        );
+        geocodingResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
+
       return;
     }
     geocodingResults.value = null;
   }, 300);
 };
 </script>
+
+<style lang="scss" scoped></style>
