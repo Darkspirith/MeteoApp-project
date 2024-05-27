850286d8eb43567ab6a4856a246e78172c428a12
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-27 12:29:00 +0200

    Add API errors handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,14 +12,23 @@
         class="absolute rounded-md bg-emerald-900 text-white w-full shadow-md py-2 px-1 top-[72px]"
         v-if="geocodingResults"
       >
-        <li
-          v-for="searchResult in geocodingResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-          @click="previewLocation(searchResult)"
-        >
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError">
+          Brak wyników. Hmm... Coś poszło nie tak. Spróbuj ponownie.
+        </p>
+        <p v-if="!serverError && geocodingResults.length === 0">
+          Niestety, nie znaleziono rezultatów dla podanej lokalizacji. Spróbuj
+          podać inną.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in geocodingResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewLocation(searchResult)"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -30,19 +39,20 @@ import axios from "axios";
 import { ref } from "vue";
 
 // Space for API token.
-const mapboxAPIKey = ""; 
+const mapboxAPIKey =
+  "";
 
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const geocodingResults = ref(null);
-const searchError = ref(false);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
       try {
         const result = await axios.get(
-          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&limit=10&types=place`
         );
         geocodingResults.value = result.data.features;
       } catch {
@@ -52,7 +62,7 @@ const getSearchResults = () => {
       return;
     }
     geocodingResults.value = null;
-  }, 300);
+  }, 320);
 };
 </script>
 
