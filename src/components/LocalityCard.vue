c81916964e50c8e63adf45a9b4a5e84b74b9e72b
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-29 22:50:07 +0200

    Add LocalityCard.vue

---
+++ src/components/LocalityCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="flex py-6 px-3 bg-meteo-secondary rounded-md shadow-md cursor-pointer"
+  >
+    <div class="flex flex-col flex-1">
+      <h2 class="text-3xl">{{ city.city }}</h2>
+      <h3>{{ city.state }}</h3>
+    </div>
+
+    <div class="flex flex-col gap-2">
+      <p class="text-3xl self-end">
+        {{ Math.round(city.weather.main.temp) }} &degC
+      </p>
+      <div class="flex gap-2">
+        <span class="text-xs">
+          Od:
+          {{ Math.round(city.weather.main.temp_min) }} &degC
+        </span>
+        <span class="text-xs">
+          Do:
+          {{ Math.round(city.weather.main.temp_max) }} &degC
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  city: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
