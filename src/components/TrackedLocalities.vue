a264136de093f2672273ac61c4d7c92bb6ffcc14
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-29 22:45:40 +0200

    Add TrackedLocalities.vue

---
+++ src/components/TrackedLocalities.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-for="city in savedLocalities" :key="city.id">
+    <LocalityCard :city="city" @click="goToLocalityView(city)" />
+  </div>
+
+  <p v-if="savedLocalities.length === 0">
+    Wygląda na to, że nie masz zapisanych lokalizacji.
+  </p>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import LocalityCard from "./LocalityCard.vue";
+
+
+// Enter OpenWeatherMap API token
+const openweathermapAPIKey = "";
+
+const savedLocalities = ref([]);
+const getLocalities = async () => {
+  if (localStorage.getItem("savedLocalities")) {
+    savedLocalities.value = JSON.parse(localStorage.getItem("savedLocalities"));
+
+    const requests = [];
+    savedLocalities.value.forEach((city) => {
+      requests.push(
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=${openweathermapAPIKey}&units=metric&lang=pl`
+        )
+      );
+    });
+
+    const meteoData = await Promise.all(requests);
+
+    meteoData.forEach((value, index) => {
+      savedLocalities.value[index].weather = value.data;
+    });
+  }
+};
+await getLocalities();
+
+const router = useRouter();
+const goToLocalityView = (city) => {
+  router.push({
+    name: "LocalityView",
+    params: { state: city.state, city: city.city },
+    query: {
+      id: city.id,
+      lat: city.coords.lat,
+      lng: city.coords.lng,
+    },
+  });
+};
+</script>
