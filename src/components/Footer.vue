6b12f94b02fcaf57e0d8ee9f6d8b9cfcafbfe650
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-30 23:05:13 +0200

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="text-center text-sm py-4 text-white bg-meteo-primary sticky top-0 shadow-emerald-900 shadow-lg ">
+    <p class="">
+      Made with <i class="fa-solid fa-heart text-meteo-secondary px-1"></i> in
+      Bytom.
+      <span class="px-10"
+        >Dane pogodowe dostarcza
+        <a class="font-bold" href="https://openweathermap.org/"
+          >OpenWeatherMap</a
+        >.</span
+      >
+      <span
+        >Geokodowanie napędza
+        <a class="font-bold" href="http://mapbox.com/about/maps">Mapbox</a>
+        .</span
+      >
+    </p>
+  </footer>
+</template>
+
+<script setup>
+</script>
