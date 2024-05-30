3c1e5743b42ccab11dc400eda3cbc1e0eeac0cdb
Darkspirith <seczkowskidominik05@gmail.com>
2024-05-30 23:12:52 +0200

    Footer update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="flex flex-col min-h-screen font-Lato bg-meteo-primary">
+  <div class="bg-meteo-primary flex flex-col min-h-screen font-Lato">
     <Navigation />
-    <RouterView />
+    <div class="flex-grow">
+      <RouterView />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style lang="scss" scoped></style>
+
+
