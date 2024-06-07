d2799968e8aca63fa202b3f855f61a0f40ad5c86
Darkspirith <seczkowskidominik05@gmail.com>
2024-06-07 23:36:43 +0200

    LocalityCard.vue - RWD update

--- src/components/LocalityCard.vue
+++ src/components/LocalityCard.vue
@@ -1,29 +1,26 @@
 <template>
   <div
-    class="flex py-4 px-4 bg-meteo-tertiary rounded-xl hover:bg-emerald-700 lg:w-screen-lg md:w-screen-md sm:w-screen-sm xs:w-screen-xs cursor-pointer"
+    class="flex flex-col p-4 bg-meteo-tertiary rounded-xl hover:bg-emerald-700 lg:w-screen-lg md:w-screen-md sm:w-screen-sm xs:w-screen-xs cursor-pointer"
   >
-    <div class="flex flex-col flex-1 ">
-      <h2 class="text-3xl">{{ city.city }}</h2>
-      <h3 class="m-1">{{ city.state }}</h3>
-    </div>
-    <div class="flex flex-col">
-      <p class="text-3xl self-end">
-        {{ Math.round(city.weather.main.temp) }} &degC
-      </p>
-      <div class="flex lg:flex-row md:flex-row sm:flex-row xs:flex-col">
-        <p class="text-sm mr-1">
-          Od:
-          {{ Math.round(city.weather.main.temp_min) }} &degC
+    <div class="flex flex-row justify-between items-center">
+      <div class="flex flex-col">
+        <h2 class="text-3xl py-1">{{ city.city }}</h2>
+        <h3 class="py-1">{{ city.state }}</h3>
+      </div>
+      <div class="flex flex-col items-end">
+        <p class="text-3xl py-2">
+          {{ Math.round(city.weather.main.temp) }} &degC
         </p>
-        <p class="text-sm lg:first-letter:lowercase md:first-letter:lowercase sm:first-letter:lowercase xs:first-letter:uppercase">
-          do:
+        <p class="text-sm text-right py-1">
+          {{ Math.round(city.weather.main.temp_min) }} &degC
+         <span class="mx-1">-</span>
           {{ Math.round(city.weather.main.temp_max) }} &degC
         </p>
       </div>
     </div>
   </div>
 </template>
- 
+
 <script setup>
 defineProps({
   city: {
